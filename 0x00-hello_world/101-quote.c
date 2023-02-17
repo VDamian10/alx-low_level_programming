9475975fdc50f49d956035816b8d2a6a376a419a
VDamian10 <victoroudora@gmail.com>
2023-02-16 17:42:25 -0800

    now

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints exactly to the standard error
+ *
+ * Return: 1
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+}
+
