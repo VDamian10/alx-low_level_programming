ee7aac85b34de4f94650521b1af084f9833597bf
VDamian10 <victoroudora@gmail.com>
2023-02-17 15:51:15 -0800

    now

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar((x % 10) + '0');
+		if (x == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
