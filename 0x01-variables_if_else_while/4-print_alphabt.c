c7616fbe368946a39887d7219623454c3865a474
VDamian10 <victoroudora@gmail.com>
2023-02-17 14:07:51 -0800

    thanks

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,8 @@ int main(void)
 	char god;
 
 	for (god = 'a'; god <= 'z'; god++)
-		putchar(god);
-	if (god != 'e' && god != 'q')
 	{
+		if (god != 'e' && god != 'q')
 		putchar(god);
 	}
 	return (0);
