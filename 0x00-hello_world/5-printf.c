c7850de594b78f2bed84e4517f426c573d31ddf2
VDamian10 <victoroudora@gmail.com>
2023-02-16 16:37:00 -0800

    now

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point to print a c program using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
