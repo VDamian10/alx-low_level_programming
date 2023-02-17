2d13f2a52743e1a503265cde19819a2c6a256e78
VDamian10 <victoroudora@gmail.com>
2023-02-17 15:25:47 -0800

    see I want to sleep

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char god;
+
+	for (god = 'z'; god >= 'a'; god++)
+		putchar(god);
+
+	putchar('\n');
+
+	return (0);
+}
