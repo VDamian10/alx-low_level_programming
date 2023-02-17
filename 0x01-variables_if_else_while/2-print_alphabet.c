eb8ee2750e4f7a0bf490bf136dcc9b80b66bd5f2
VDamian10 <victoroudora@gmail.com>
2023-02-17 12:06:27 -0800

    wow

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char god;
+
+	for (god = 'a'; god <= 'z'; god++)
+		putchar(god);
+
+	putchar('\n');
+
+	return (0);
+}
