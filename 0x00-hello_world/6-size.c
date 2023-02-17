ac9498cee42f25689530e8ab06e06afc6e54c58c
VDamian10 <victoroudora@gmail.com>
2023-02-16 16:49:21 -0800

    help

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a line of code to print out the size of types on a computer
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	double d;
+
+	sizeof(c);
+	sizeof(i);
+	sizeof(d);
+
+	return (0);
+}
