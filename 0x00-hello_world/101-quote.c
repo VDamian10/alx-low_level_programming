e4262d069c6fdc4ab45e368cd53571cc98cb07af
VDamian10 <victoroudora@gmail.com>
2023-02-20 06:25:41 -0800

    wow

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints exactly to the standard error
  *
@@ -6,7 +7,9 @@
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", text);
+
 	return (1);
 }
-
