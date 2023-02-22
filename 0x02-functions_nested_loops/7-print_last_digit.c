b54bb9a7fb9825b07e3d8334103c006ae14ee8c8
VDamian10 <victoroudora@gmail.com>
2023-02-21 19:40:54 -0800

    i have waited

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,11 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	if (n <= 0 || n >= 0)
-		_putchar(last_digit);
+	if (n < 0)
+		last_digit *= -1;
+
+	else if (n < 0)
+		last_digit *= 1;
 
 	return (last_digit);
 }
