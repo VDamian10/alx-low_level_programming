ecaf7d4315eec8a20f44faaf0ab091c59b07c757
VDamian10 <victoroudora@gmail.com>
2023-02-22 14:10:49 -0800

    yes

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - entry
+ * @n: random whole number less than 98
+ *
+ * this program prints all whole numbers between n and 98
+*/
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d", n++);
+
+		}
+	printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d", n--);
+		}
+	printf("%d\n", n);
+	}
+}
