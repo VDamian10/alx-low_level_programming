6b84e68780bf4dffcc0f48114b779279be136b5a
VDamian10 <victoroudora@gmail.com>
2023-02-22 07:34:07 -0800

    now

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,12 +14,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	if (n == 0)
+	else
 	{
 		_putchar('0');
 		return (0);
