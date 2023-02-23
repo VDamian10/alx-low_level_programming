55c5bea5ba4bd73c4a40e420f234c54ee2dfc738
VDamian10 <victoroudora@gmail.com>
2023-02-23 15:17:48 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - entry point
+ *
+ * @size: integer variable
+ * prints 8 square
+ *
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
