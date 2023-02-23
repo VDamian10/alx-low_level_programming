74ca6b0ca79b5e17613aae54ab62e999b10a9abe
VDamian10 <victoroudora@gmail.com>
2023-02-23 08:00:01 -0800

    i think i did it

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,29 +11,38 @@ void print_times_table(int n)
 {
 	int num1, num2, answer;
 
-	if (n <= 15 && n >= 0)
+	if (n > 15 || n < 0)
 	{
 		for (num1 = 0; num1 <= n; num1++)
 		{
 			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
 
 			for (num2 = 1; num2 <= n; num2++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
 				answer = num1 * num2;
 
-				if (answer >= 10 && answer <= 99)
+				if (answer < 9)
+				{
+					_putchar((answer % 10) + '0');
+				}
+
+				else if (answer >= 10 && answer <= 99)
 				{
 					_putchar((answer / 10) + '0');
+					_putchar((answer % 10) + '0');
 				}
-				else if (answer >= 100)
+
+				else
 				{
-					_putchar((answer / 100) + '0');
-					_putchar(((answer / 10) % 10) + '0');
+					if (answer >= 100)
+					{
+						_putchar((answer / 100) + '0');
+						_putchar(((answer / 10) % 10) + '0');
+						_putchar((answer % 10) + '0');
+					}
 				}
-				_putchar((answer % 10) + '0');
 			}
 			_putchar('\n');
 		}
