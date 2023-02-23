a2b9e993abe895c6fcaa9bc2fec5c687ac903987
VDamian10 <victoroudora@gmail.com>
2023-02-23 12:43:50 -0800

    abeg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 {
 	int n, tens, units;
 
-	for (n = 0; n < 15; n++)
+	for (n = 0; n <= 14; n++)
 	{
 		if (n >= 10)
 		{
-			_putchar((n / 10) + '0');
+			_putchar('1');
 		}
 		_putchar((n % 10) + '0');
 	}
