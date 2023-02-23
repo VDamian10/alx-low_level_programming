f9c5439adcf358c27f6aa534ba4711b98bab0bcd
VDamian10 <victoroudora@gmail.com>
2023-02-23 13:26:54 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ *
+ * @n: number of times
+ *
+ * a function that drasw a straight line
+ *
+*/
+void print_line(int n)
+{
+	int v;
+
+	for (v = 0; v <= n; v++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
