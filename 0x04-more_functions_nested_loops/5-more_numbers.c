a1933b631f02778b7a377e8525b5b75da7046118
VDamian10 <victoroudora@gmail.com>
2023-02-23 12:31:50 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - entry point
+ *
+ * prints 10 times the numbers, from 0 to 14, followed by a new line
+ *
+*/
+void more_numbers(void)
+{
+	int n, tens, units;
+
+	for (n = 0; n <= 14; n++)
+	{
+		tens = n / 10;
+		units = n % 10;
+
+		if (n >= 10)
+		{
+			_putchar(tens + '0');
+		}
+		_putchar(units + '0');
+	}
+	_putchar('\n');
+}
