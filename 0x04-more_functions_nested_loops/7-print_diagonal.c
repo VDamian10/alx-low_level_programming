8bfefcc4b2859e1e55912e749a5d718209c68b51
VDamian10 <victoroudora@gmail.com>
2023-02-23 13:38:01 -0800

    wow

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - entry point
+ * @n: number of times \ should be printed
+ *
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar ('\\');
+		_putchar('\n');
+	}
+
+}
