872b633b84cb817a2a28271e244eda3ce5814c28
VDamian10 <victoroudora@gmail.com>
2023-02-22 17:15:47 -0800

    times table for number n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - entry point
+ *
+ * @n: number to have a times table built out for
+ *
+ * this program prints the times table of value n
+ *
+*/
+void print_times_table(int n)
+{
+	int num1, num2, answer;
+
+	if (n > 15 || n < 0)
+	{
+		for (num1 = 0; num1 <= n; num1++)
+		{
+			_putchar('0');
+
+			for (num2 = 1; num2 <= n; num2++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				answer = num1 * num2;
+
+				if (answer < 9)
+					_putchar(' ');
+
+				else if (answer >= 10 || answer <= 99)
+				{
+					_putchar((answer / 10) + '0');
+				}
+				else
+				{
+					_putchar((answer % 100) + '0');
+					_putchar((answer / 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
