cd810327f050d66a414925022b0338ced14f2663
VDamian10 <victoroudora@gmail.com>
2023-02-27 12:14:35 -0800

    lengh of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - entry point
+ * @s: string
+ *
+ * a function that checks the length of a string
+ *
+ * Return: length
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; length <= *s; length++)
+	{
+		*s++;
+		return (length);
+	}
+}
