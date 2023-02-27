3f2da3cc15e7f724da4644c727741337c6176d25
VDamian10 <victoroudora@gmail.com>
2023-02-27 13:45:13 -0800

    prints a string and new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - entry
+ * @str: string
+ * a function that prints a string followed by a new line
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+	{
+		str++;
+	}
+	_putchar('\n');
+}
