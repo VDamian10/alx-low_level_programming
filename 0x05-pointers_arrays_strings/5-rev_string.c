c9da12a9d0905f08364afd90712fa886e837b82c
VDamian10 <victoroudora@gmail.com>
2023-02-27 17:30:55 -0800

    yes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rev_string - entry point
+ *
+ * @s: string
+ * function to print a string in reverse
+*/
+void rev_string(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	j = 0;
+
+	while (*s != '\0')
+	{
+		j++;
+		s++;
+	}
+
+	for (j = j - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+}
