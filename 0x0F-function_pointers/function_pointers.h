a39c1855ed4f415d966f06ef3a89887f4d11d119
VDamian10 <victoroudora@gmail.com>
2023-03-24 20:06:20 -0700

    started

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *n));
 int _putchar(char c);
-void pme (char *name);
-int str_len(char *n);
 void array_iterator(int *array, size_t size, void (*action)(int));
 void prim(int a);
 int int_index(int *array, int size, int (*cmp)(int x));
