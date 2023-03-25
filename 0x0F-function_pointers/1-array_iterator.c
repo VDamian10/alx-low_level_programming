6bc183fea13599dfc316fff38c951778506a5dd1
VDamian10 <victoroudora@gmail.com>
2023-03-24 20:19:19 -0700

    started

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * array_iterator - searches for an integer
- * @array: name of array of integers
- * @size: size of array
+ * @array: array of integers
+ * @size: size of the array
  * @action: pointer to a function
  * Return: nothing
-*/
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int o = 0;
+	size_t i = 0;
 
-	while (o < size)
+	if (array && action)
 	{
-		action(array[o]);
-		o++;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 	}
 }
-
