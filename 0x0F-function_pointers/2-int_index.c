6046bd5d57d6765bf3fac9696daa9c37471015e7
VDamian10 <victoroudora@gmail.com>
2023-03-24 20:22:13 -0700

    started

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,47 +1,28 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
- * cmpme - compares values
- * @x: integer to compare
- * Return: Always 0 unless there's a match
-*/
-int cmpme(int x)
+  * int_index - searches for an integer
+  * @array: array of integers
+  * @size: size of the array
+  * @cmp: pointer to a function
+  * Return: index
+  */
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (x != 2)
-		return (0);
+	int i = 0;
 
-	else
-		;
-}
-
-/**
- * int_index - returns the index of the first match
- * @array: array of integers
- * @size: size of the array
- * @cmp: pointer to _cmpme
- * Return: index of the match
-*/
-int int_index(int *array, int size, int (*cmp)(int x))
-{
-	int i = 0, key = 2;
-	cmp = &cmpme;
-
-	if (array == NULL || cmp == NULL || size <= 0)
-		return (-1);
-
-	while (i < size)
+	if (size > 0)
 	{
-		cmp(array[i]);
-
-		if (array[i] == key)
+		if (array != NULL && cmp != NULL)
 		{
-			return (i);
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
 		}
-
-		else
-			return (-1);
-
-		i++;
 	}
 
+	return (-1);
 }
