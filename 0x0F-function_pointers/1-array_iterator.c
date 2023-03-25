ab527700bed7c1d9eae4219e361e3de02e518aad
VDamian10 <victoroudora@gmail.com>
2023-03-24 20:10:38 -0700

    started

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,27 +1,15 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- * prim - multiplies an integer by 2
- * @a: integer
- * Return: nothing 
+ * array_iterator - searches for an integer
+ * @array: name of array of integers
+ * @size: size of array
+ * @action: pointer to a function
+ * Return: nothing
 */
-void prim(int a)
-{
-	int prod = a * 2;
-
-	if (a == 0)
-		return;
-}
-
-
-/**
- * array_iterator
-*/
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int o = 0;
-
-	action = &prim;
 
 	while (o < size)
 	{
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,6 @@
 void print_name(char *name, void (*f)(char *n));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
-void prim(int a);
 int int_index(int *array, int size, int (*cmp)(int x));
 int cmpme(int x);
 #endif
