c175bb9f80dae4bbf9578d9fec62fb57b6e852d4
VDamian10 <victoroudora@gmail.com>
2023-04-05 11:00:01 -0700

    try again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newnode->n = n;
 
-	if (idx == 0 || !*head)
+	if (idx == 0)
 	{
 		newnode->next = *head;
 		*head = newnode;
