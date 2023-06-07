3c18c0778b822f6e412ec8ea6819955e5a2a3357
VDamian10 <victoroudora@gmail.com>
2023-06-07 22:28:24 +0100

    fixed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,14 +9,14 @@
 
 int is_prime_number(int n)
 {
-	int count = 0;
+	int i, count = 0;
 
 	if (n <= 1)
 	{
 		return (0);
 	}
 
-	for (int i = 1; i <= n / 2; i++)
+	for (i = 1; i <= n / 2; i++)
 	{
 		if ((n % i) == 0)
 		{
