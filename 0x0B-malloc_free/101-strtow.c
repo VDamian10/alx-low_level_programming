b76bbdc63a426ef2ae10b6d3581eb9c2f204f3b9
VDamian10 <victoroudora@gmail.com>
2023-06-15 03:47:09 +0100

    split a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,28 +1,78 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
+ * checkwords - counts number of words
+ * @str: string to be checked
+ * Return: number of words
 */
+
+int checkwords(char *str)
+{
+	if (!str)
+		return (0);
+
+	int count = 0, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && str[i - 1] != ' ')
+		{
+			count++;
+			i++;
+		}
+	}
+
+	count++;
+	return (count);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string to be split
+ * Return: pointer to a pointer
+*/
+
 char **strtow(char *str)
 {
-	if (!str || *str == "")
+	if (!str || *str == '\0')			/* checking if str is NULL or an empty space */
 		return (NULL);
 
-	char **newstr = malloc(sizeof(char *) * slen(str));
-	
-	if (!newstr)
+	int length, y, j, start, x = 0;
+
+	char **strarray = malloc(sizeof(char *) * checkwords(str) + 1);
+
+	if (!strarray)
 		return (NULL);
-	
-	int i = 0;
-	
-	while (str[i] != '\0')
+
+	for (j = 0; str[j] != '\0'; j++)		/* storing words in prev allocated memory */
 	{
-		if (str[i] == " ")
+		if (str[j] != ' ')			/* handling only chars that are not spaces */
 		{
-			newstr[
-		}
+			start = j;			/* setting starting point for first word */
 
-		i++;
+			while (str[j] != ' ' && str[j] != '\0')			/*incrementing to the next word */
+				j++;
+
+			length = j - start;			/* determining length of the word to store */
+			strarray[x] = malloc(sizeof(char) * length + 1);
+
+			if (!strarray[x])
+			{
+				for (y = 0; y < x; y++)
+					free(strarray[y]);
+
+				free(strarray);
+				return (NULL);
+			}
+
+			strncpy(strarray[x], &str[start], length);	/* copying into array element */
+			strarray[x][length] = '\0';
+			x++;
+		}
 	}
+	strarray[x] = NULL;			/* assigning last element of the array */
+	return (strarray);
 }
