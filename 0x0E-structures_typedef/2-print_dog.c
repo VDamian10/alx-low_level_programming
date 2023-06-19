58b0ac5f8c7e802ae9a6aeb93acca5a9a72e97d0
VDamian10 <victoroudora@gmail.com>
2023-06-19 22:09:22 +0100

    printed a proper struct for all cases

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,43 +1,29 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
  * print_dog - prints new struct for dog
  * @d: name of struct
  *
  * Return: void
 */
+
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
 		if (d->name != NULL)
-		{
 			printf("Name: %s\n", d->name);
-		}
 
 		else
-		{
 			printf("Name: (nil)");
-		}
 
-		if (d->age != 0)
-		{
-			printf("Age: %f\n", d->age);
-		}
-
-		else
-		{
-			printf("Age: (nil)");
-		}
+		printf("Age: %f\n", (d->age));
 
 		if (d->owner != NULL)
-		{
 			printf("Owner: %s\n", d->owner);
-		}
 
 		else
-		{
 			printf("Owner: (nil)");
-		}
 	}
 }
