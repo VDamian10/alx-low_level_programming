35e9ace6233bb430307cc416e8e3e47c80be1b17
VDamian10 <victoroudora@gmail.com>
2023-06-21 19:20:01 +0100

    printed a name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *n));
+int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int x));
+int cmpme(int x);
+#endif
