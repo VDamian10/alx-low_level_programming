c507b78992b8cebe7ba51fa872b6afb108fa2e73
VDamian10 <victoroudora@gmail.com>
2023-06-23 01:15:04 +0100

    printed strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list param;
-	char *nil= "(nil)", *str;
+	char *nil = "(nil)", *str;
 
 	if (!separator)
 		separator = "";
