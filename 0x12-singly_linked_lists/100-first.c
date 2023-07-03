ae898a0e595348d1fec477ed4ef79e42e365040d
VDamian10 <victoroudora@gmail.com>
2023-07-04 00:04:24 +0100

    printing before the main func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * beforemain - prints a sentence before exe of main function
+ * Return: nothing
+*/
+
+void beforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
