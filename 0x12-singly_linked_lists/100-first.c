2643c9f3f1a1af41e631b204ed45478cbdf8c014
VDamian10 <victoroudora@gmail.com>
2023-07-04 00:13:06 +0100

    made changes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,7 @@ void __attribute__((constructor)) beforemain(void);
 
 void beforemain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf(
+			"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+			);
 }
