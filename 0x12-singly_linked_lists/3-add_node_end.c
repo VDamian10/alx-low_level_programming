5d70655d06b81e574f25edfdb35a41cb4814f60e
VDamian10 <victoroudora@gmail.com>
2023-07-03 22:47:19 +0100

    we can add nodes at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,86 @@
+#include "lists.h"
+
+/**
+ * strlength - calculates string length
+ * @str: string
+ * Return: length
+*/
+unsigned int strlength(const char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * cpme - duplicates a string
+ * @str: string to be duplicated
+ * Return: pointer to a string
+*/
+
+char *cpme(const char *str)
+{
+	char *new;
+	unsigned int i, j, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	new = malloc(sizeof(char) * len);
+	if (!new)
+		return (NULL);
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		new[j] = str[j];
+	}
+
+	new[j] = '\0';
+	return (new);
+}
+
+/**
+ * add_node_end - adds a new node to the end
+ * @head: head of the list
+ * @str: string to be added
+ * Return: pointer to the new node
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode;
+	list_t *trav;
+
+	if (!*head)
+	{
+		newnode = malloc(sizeof(list_t));
+		if (!newnode)
+			return NULL;
+
+		newnode->str = cpme(str);
+		newnode->len = strlength(newnode->str);
+		newnode->next = NULL;
+		*head = newnode;
+	}
+
+	trav = *head;
+	while (trav->next)
+		trav = trav->next;
+
+	newnode = malloc(sizeof(list_t));
+	if (!newnode)
+		return (NULL);
+
+	newnode->str = cpme(str);
+	newnode->len = strlength(newnode->str);
+	newnode->next = NULL;
+	trav->next = newnode;
+
+	return (newnode);
+}
