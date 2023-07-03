1e694a43dd663057324b7ac5c08231ff51dcfd29
VDamian10 <victoroudora@gmail.com>
2023-07-03 21:56:48 +0100

    added a node and updated header file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,79 @@
+#include "lists.h"
+
+/**
+ * strlength - calculates string length
+ * @str: string
+ * Return: length
+*/
+unsigned int strlength(const char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * cpme - duplicates a string
+ * @str: string to be duplicated
+ * Return: pointer to a string
+*/
+
+char *cpme(const char *str)
+{
+	char *new;
+	unsigned int i, j, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	new = malloc(sizeof(char) * len);
+	if (!new)
+		return (NULL);
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		new[j] = str[j];
+	}
+
+	new[j] = '\0';
+	return (new);
+
+}
+
+/**
+ * add_node - adds a new node at the beginning
+ * @head: pointer to a pointer
+ * @str: string
+ * Return: pointer to a struct
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	if (!head)
+		return (NULL);
+
+	newnode = malloc(sizeof(list_t));
+	if (!newnode)
+		return (NULL);
+
+	newnode->str = cpme(str);
+	if (!newnode->str)
+	{
+		free(newnode);
+		return (NULL);
+	}
+
+	newnode->len = strlength(newnode->str);
+	newnode->next = *head;
+	*head = newnode;
+
+	return (newnode);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
+char *cpme(const char *str);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
