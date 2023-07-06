5d7f177432540100678035a3422caeb75de05494
VDamian10 <victoroudora@gmail.com>
2023-07-06 01:52:58 +0100

    changes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,38 +15,35 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!*head || idx <= 0)
 		return (NULL);
 
-	newnode = malloc(sizeof(listint_t));
-	if (!newnode)
-		return (NULL);
-
-	newnode->n = n;
-	newnode->next = NULL;
-
 	if (idx == 0)
 	{
+		newnode = malloc(sizeof(listint_t));
+		if (!newnode)
+			return (NULL);
+		newnode->n = n;
 		newnode->next = *head;
 		*head = newnode;
 		return (newnode);
 	}
 	trav = *head;
-	while (trav)
+	while (trav && key < idx)
 	{
 		if (key == idx - 1)
 		{
+			newnode = malloc(sizeof(listint_t));
+			if (!newnode)
+				return (NULL);
+			newnode->n = n;
 			newnode->next = trav->next;
 			trav->next = newnode;
 			return (newnode);
 		}
-
 		if (!trav->next)
 		{
-			free(newnode);
 			return (NULL);
 		}
-
 		trav = trav->next;
 		key++;
 	}
-	free(newnode);
 	return (NULL);
 }
