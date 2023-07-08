46962d853716e5a066e5d35a0bb9b72bd40ed472
VDamian10 <victoroudora@gmail.com>
2023-07-08 02:13:07 +0100

    converted from binary to dec

---
+++ 0x14-bit_manipulation/proto.h
@@ -0,0 +1,12 @@
+#ifndef PROTO_H
+#define PROTO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+#endif
