33b1f669ce1acea8b15abc6575c15a60eb82619d
VDamian10 <victoroudora@gmail.com>
2023-08-08 22:42:50 +0100

    copied and done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,72 @@
 #include "main.h"
 
 /**
+ * closeme - closes a file descriptor
+ * @x: file descriptor
+ * Return: nothing
+*/
+
+void closeme(int x)
+{
+	int closed;
+
+	if (x > 0)
+		closed = close(x);
+
+	if (closed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", x);
+		exit(100);
+	}
+}
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 if successful or -1
 */
 
 int main(int argc, char *argv[])
 {
+	int cp1, cp2;
+	const char *file_to, *file_from;
+	char buffer[1024];
+	ssize_t readme, writeme;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = argv[1];
+	file_to = argv[2];
+
+	cp1 = open(file_from, O_RDONLY);
+	if (cp1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	cp2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (cp2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((readme = read(cp1, buffer, 1024)) > 0)
+	{
+		writeme = write(cp2, buffer, readme);
+		if (writeme == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	closeme(cp1);
+	closeme(cp2);
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,12 +3,6 @@
 
 #include <stdio.h>
 #include <unistd.h>
-
-#include <stdio.h>
-#include <unistd.h>
-
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -17,6 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void closeme(int x);
+int main(int argc, char *argv[]);
 
 #endif
